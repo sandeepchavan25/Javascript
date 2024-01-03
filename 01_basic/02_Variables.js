b828ac91e63020c81c3352dc76afb7df5c6e1241
sandeepchavan25 <143996262+sandeepchavan25@users.noreply.github.com>
2024-01-03 06:22:16 +0000

    datatypes

---
+++ 01_basic/02_Variables.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3)   // we are using node js not browser
+
+let name="sandeep"
+let age=21
+let isLogged=false
+
+// primitive datatypes
+// number == 2 to power 53
+// bigint
+// string === ""
+// boolean== true/false
+// null == standalone
+// undefined ==
+// symbol
+
+console.log(typeof undefined);
+console.log(typeof null);
