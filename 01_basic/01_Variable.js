7cdd5dd654d8e12b40c6168eb6c89259d82af08a
sandeepchavan25 <143996262+sandeepchavan25@users.noreply.github.com>
2024-01-03 05:59:44 +0000

    containercommit

---
+++ 01_basic/01_Variable.js
@@ -0,0 +1,18 @@
+const accountId=1234
+let accountEmail="sandipchavan42705@gmail.com"
+var accountPassword="12345"
+accountCity="Atpadi"
+let accountState;
+
+//accountId=2
+accountEmail="prajyotsurwase@gmail.com"
+accountPassword="9876"
+accountCity="sangola"
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+/*
+
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
