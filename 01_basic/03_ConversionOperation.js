a35cac02433e0b3e14b37ffbeb74eae917a22bc8
sandeepchavan25 <143996262+sandeepchavan25@users.noreply.github.com>
2024-01-03 07:41:50 +0000

    conversion

--- 01_basic/03_ConversionOperation.js
+++ 01_basic/03_ConversionOperation.js
@@ -14,5 +14,35 @@ let score=true
 let someNumber=33;
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************operations**********************************
+
+// let value=3
+// let negValue= -value
+// console.log(negValue);
+
+// let str1="sandeep"
+// let str2=" good morning"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log("3"+3);
+// console.log(3+"3");
+// console.log(3+3+3);
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3=2+2
+console.log(num2);
+
+let gameCounter=100
+++gameCounter;
+
+console.log(gameCounter)
