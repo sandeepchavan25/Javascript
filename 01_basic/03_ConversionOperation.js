59eb1857d48a327cc019ebc351cee905ad13f3d9
sandeepchavan25 <143996262+sandeepchavan25@users.noreply.github.com>
2024-01-03 06:43:19 +0000

    conversion of datatypes

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,18 @@
+let score=true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn=false;
+// let booleanisLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+
+let someNumber=33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
